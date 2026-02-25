ceede2cb6f6a71dc937902ee3cb80764b22f7cdb
musabnage-beep <musabnage@gmail.com>
2026-02-24 19:58:09 -0800

    Update server.js

--- server.js
+++ server.js
@@ -25,11 +25,12 @@ app.use(express.static(__dirname));
 // DATABASE CONNECTION
 // ===========================
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'better',
-    charset: 'utf8mb4'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  charset: 'utf8mb4'
 });
 
 // Connect to database
